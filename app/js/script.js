f8e0f734bc96e0aed2351f582046c93033bbf615
Navin <lancidan@yahoo.com>
2020-10-06 02:20:04 +0530

    Added animated mobile menu

--- app/js/script.js
+++ app/js/script.js
@@ -1,16 +1,24 @@
+const body = document.querySelector('body');
 const btnHamburger = document.querySelector('#btnHamburger');
 const header = document.querySelector('.header');
 const overlay = document.querySelector('.overlay');
+const fadeElems = document.querySelectorAll('.has-fade');
 
 btnHamburger.addEventListener('click', (event) => {
     console.log('click hamburger');
     if(header.classList.contains('open')) { // close hamburger menu
         header.classList.remove('open');
-        overlay.classList.add('fade-out');
-        overlay.classList.remove('fade-in');
+        fadeElems.forEach((element) => {
+            element.classList.add('fade-out');
+            element.classList.remove('fade-in');
+        });
+        body.classList.remove('noscroll');
     } else { // Open hamburger menu
         header.classList.add('open');
-        overlay.classList.remove('fade-out');
-        overlay.classList.add('fade-in');
+        fadeElems.forEach((element) => {
+            element.classList.remove('fade-out');
+            element.classList.add('fade-in');
+        });
+        body.classList.add('noscroll');
     }
 });
